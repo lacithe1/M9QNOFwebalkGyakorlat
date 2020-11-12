3925734a8f42a490ccc3494b164afbeb0203923e
Kovács László <k.laszloo97@gmail.com>
2020-11-12 01:54:55 +0100

    dwerf5v23v

---
+++ M9QNOF_20201105/m9qnofspringbootweb/src/main/java/hu/m9qnof/springtbootweb/M9QnoFspringbootwebApplication.java
@@ -0,0 +1,13 @@
+package hu.m9qnof.springtbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M9QnoFspringbootwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M9QnoFspringbootwebApplication.class, args);
+	}
+
+}
---
+++ M9QNOF_20201105/m9qnofspringbootweb/src/test/java/hu/m9qnof/springtbootweb/M9QnoFspringbootwebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.m9qnof.springtbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M9QnoFspringbootwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
